1991631d7cf2baf23ffb111cb4919c79511aa299
Uzair Ali <uzairali19b@gmail.com>
2021-12-18 17:40:58 +0500

    update GET POST and DELETE

--- src/redux/books/booksReducer.js
+++ src/redux/books/booksReducer.js
@@ -1,16 +1,62 @@
 const ADD_BOOK = 'bookStore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
+const GET_BOOK = 'bookStore/books/GET_BOOK';
 const initialState = [];
+const api = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/9gdLqfeWisFSvgsvAsnF/books';
 
-export const addBook = (payload) => ({
-  type: ADD_BOOK,
-  payload
-});
+export const getBook = () => async (res) => {
+  const response = await fetch(api).then((data) => {
+    return data.json();
+  });
+  const books = [];
+  Object.keys(response).forEach((key) => {
+    books.push({ id: key, ...response[key][0] });
+  });
+  if (books) {
+    res({
+      type: GET_BOOK,
+      payload: books
+    });
+  }
+};
 
-export const removeBook = (payload) => ({
-  type: REMOVE_BOOK,
-  payload
-});
+export const addBook = (payload) => async (res) => {
+  const response = await fetch(api, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      item_id: payload.id,
+      title: payload.title,
+      category: payload.author
+    })
+  });
+  if (response) {
+    res({
+      type: ADD_BOOK,
+      payload
+    });
+  }
+};
+
+export const removeBook = (payload) => async (res) => {
+  const response = await fetch(`${api}/${payload}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-type': 'application/json'
+    },
+    body: JSON.stringify({
+      item_id: payload.id
+    })
+  });
+  if (response) {
+    res({
+      type: REMOVE_BOOK,
+      payload
+    });
+  }
+};
 
 export const booksReducer = (state = initialState, action) => {
   switch (action.type) {
@@ -18,6 +64,8 @@ export const booksReducer = (state = initialState, action) => {
       return [...state, action.payload];
     case REMOVE_BOOK:
       return [...state.filter((book) => book.id !== action.payload)];
+    case GET_BOOK:
+      return action.payload;
     default:
       return state;
   }
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,11 +1,12 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
 import { booksReducer } from './books/booksReducer';
 
 const reducer = combineReducers({
   booksReducer
 });
 
-const store = createStore(reducer, applyMiddleware(logger));
+const store = createStore(reducer, applyMiddleware(thunkMiddleware, logger));
 
 export default store;
